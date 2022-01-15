775ca9a5d314b4dbefdb2347208eda73461512a9
tanmay <patelt9238@gmail.com>
2022-01-15 23:19:57 +0530

    added list features

--- app.js
+++ app.js
@@ -4,37 +4,33 @@ const bodyParser=require("body-parser");
 
 const app=express();
 
+var items=["buy food","cook food","eat food"];
+
 app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
 
     var today=new Date();
-    var currentday=today.getDay();
-    var day="";
+    
+    var options ={
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    if(currentday==0){
-        day="sunday";
-    }
-    else if(currentday==1){
-        day="monday";
-    }
-    else if(currentday==2){
-        day="tuesday";
-    }
-    else if(currentday==3){
-        day="wednesday";
-    }
-    else if(currentday==4){
-        day="thursday";
-    }
-    else if(currentday==5){
-        day="friday";
-    }
-    else{
-        day="saturday";
-    }
-    res.render("list", {kindOfDay: day});
+    var day=today.toLocaleDateString("en-US",options);
+
+    res.render("list", {kindOfDay: day,newListItems:items});
 });
+
+app.post("/",function(req,res){
+    var item=req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+})
 
 app.listen(3000,function(){
     console.log("server started on port 3000");
