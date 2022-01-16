4263e3b155cfeb634280afb37acdccbc08130a0e
tanmay <patelt9238@gmail.com>
2022-01-16 05:56:21 +0530

    added css and made another work page

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser=require("body-parser");
 const app=express();
 
 var items=["buy food","cook food","eat food"];
+let workItems=[];
 
 app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
 
@@ -22,15 +24,32 @@ app.get("/",function(req,res){
 
     var day=today.toLocaleDateString("en-US",options);
 
-    res.render("list", {kindOfDay: day,newListItems:items});
+    res.render("list", {listTitle: day,newListItems:items});
 });
 
 app.post("/",function(req,res){
     var item=req.body.newItem;
-    items.push(item);
+    
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+});
 
-    res.redirect("/");
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List",newListItems:workItems});
+});
+
+app.post("/work",function(req,res){
+    let item=req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 })
+
 
 app.listen(3000,function(){
     console.log("server started on port 3000");
